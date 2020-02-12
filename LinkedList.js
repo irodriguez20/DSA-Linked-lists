cefcf9626301f9cb09c4ff1ec78e7ca03353f06b
India Rodriguez <carnes.s.ia17@gmail.com>
2020-02-11 23:33:41 -0700

    completed creating a singly linked list, still have several more exercises to complete

--- LinkedList.js
+++ LinkedList.js
@@ -1,14 +1,57 @@
+class _Node {
+    constructor(value, next) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
 class LinkedList {
     constructor() {
         this.head = null;
     }
+
     /* An item can be inserted, at the beginning of the list(insertFirst)
-    at the end of the list(insertLast)
-    or anywhere in the list, between 2 nodes (insert, insertAt)
-    */
+      at the end of the list(insertLast)
+      or anywhere in the list, between 2 nodes (insert, insertAt)
+      */
 
     insertFirst(item) {
         this.head = new _Node(item, this.head);
+    }
+
+    insertBefore(item, item2) {
+        let currNode = this.head;
+        let prevNode = null;
+        while (currNode.next !== null) {
+
+            if (item2 === currNode.value) {
+                // console.log(currNode.value);
+                // console.log(prevNode.value);
+                const newNode = new _Node(item, currNode);
+                prevNode.next = newNode;
+                break;
+            }
+
+            prevNode = currNode;
+            currNode = currNode.next;
+        }
+
+    }
+
+    insertAfter(item, item2) {
+        let currNode = this.head;
+        let nextNode = null;
+        while (currNode.next !== null) {
+            if (item2 === currNode.value) {
+                console.log(currNode.value);
+                console.log(nextNode.value);
+                const newNode = new _Node(item, nextNode);
+                currNode.next = newNode;
+                break;
+            }
+            nextNode = currNode.next.next;
+            currNode = currNode.next;
+        }
     }
 
     insertLast(item) {
@@ -21,6 +64,26 @@ class LinkedList {
                 tempNode = tempNode.next;
             }
             tempNode.next = new _Node(item, null);
+        }
+    }
+
+    insertAt(item, position) {
+        let currNode = this.head;
+        let count = null;
+        let nextNode = null;
+        console.log(count)
+        while (currNode.next !== null) {
+            count = position
+            if (count === 0) {
+                this.insertFirst(item)
+            }
+            else {
+                currNode.next.next = new _Node(item, currNode.next.next)
+                break;
+            }
+
+            nextNode = currNode.next.next;
+            currNode = currNode.next;
         }
     }
 
@@ -73,3 +136,27 @@ class LinkedList {
         previousNode.next = currNode.next;
     }
 }
+
+function main() {
+    const SLL = new LinkedList();
+    SLL.insertLast("Apollo");
+    SLL.insertLast("Boomer");
+    SLL.insertLast("Helo");
+    SLL.insertLast("Husker");
+    SLL.insertLast('Starbuck');
+    SLL.insertLast("Tauhida");
+    SLL.remove("Husker");
+    SLL.insertBefore("Athena", "Boomer");
+    SLL.insertAfter("Hotdog", "Helo");
+    SLL.insertAt("Kat", 3);
+    SLL.remove("Tauhida");
+    console.log(SLL);
+
+}
+
+
+
+
+console.log(main())
+
+
